b928502ba78e7031c850020f817f9567c1ef73ca
Sanjiban Barik <sanjibanbarik008@gmail.com>
2024-03-02 21:49:24 +0530

    utils code added

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors"
+
+
+const app = express()
+app.use(cors({
+    origin : process.env.CORS_ORIGIN, 
+    credentials : true
+}))
+
+
+app.use(express.json( { limit: '16kb' })) // to support JSON-encoded bodies
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); // to support URL-
+app.use(express.static("public")) // for serving static files like images,
+app.use(cookieParser()) // to support cookies in requests and responses
+
+export {app}
--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 import dotenv  from 'dotenv';
 import connectDB from "./db/index.js";
+import { app  } from './app.js'
 
 dotenv.config({
     path: './env'
 })
 
 connectDB()
+.then(()=> {
+    app.listen(process.env.PORT || 8000, ()=> {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err)=> {
+    console.log("MONGODB connection failde!! ", err);
+})
 
-// ( async () => {
-//     try {
-//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-//     } catch(error){
-//         console.error("ERROR: ", error)
-//         throw error
-//     }
-// })()
+
 
 
 
